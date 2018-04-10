1ea5ec06e6af1a6a7d3b1d73cacfad79134855a7
Darwin Gonzalez Suarez <alu0100812794@ull.edu.es>
2018-04-10 09:58:46 +0000

    Lectura fichero de texto

--- methods.js
+++ methods.js
@@ -6,11 +6,16 @@ var server = restify.createServer();
 
 var texto ="";
 
-server.listen(8888, function() {
+var text = fs.readFileSync('data.txt','utf8')
+texto = text.split('\n');
+console.log(texto);
+
+
+server.listen(process.env.PORT, function() {
     console.log(' %s escuchando en el puerto %s', server.name, server.url);
 });
 
 server.on('unCaughtException', (resq, res, route, err) => {
     err.body = 'He pillado una exception :(';
     res.send(500, err);
-})
+});
