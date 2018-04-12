19f7fec0541aafc8c01e51debaaa907bdcff0dc9
Darwin Gonzalez Suarez <alu0100812794@ull.edu.es>
2018-04-12 12:41:42 +0000

    Añadiendo buscar fila

--- methods.js
+++ methods.js
@@ -5,18 +5,40 @@ var fs = require('fs');
 var server = restify.createServer();
 
 var texto ="";
-var nid=100, valor= 'informatica';
+var nid = 2, valor = "fisica";
+var fila = 0
 
 var text = fs.readFileSync('data.txt','utf8')
 texto = text.split('\n');
 console.log(texto);
+console.log("Escribe tu nombre");
 
+  
 fs.appendFile('data.txt', addFila(nid, valor), 'utf8');
 
 function addFila(nid, valor) {
     var aux = "{ \"nid\":\"" + nid + "\"," + "\"valor\":\"" + valor + "\" }\n";
     return aux;
 }
+
+function findFilabyNid(nid, valor) {
+    var text = fs.readFileSync('data.txt','utf8')
+    texto = text.split('\n');
+    var aux = "{ \"nid\":\"" + nid + "\"," + "\"valor\":\"" + valor + "\" }\n";
+    
+    for(var i = 0; i <= texto.length ; i++) {
+        if (texto[i] == aux) {
+            console.log("Encontrado!");
+        }
+        else {
+            console.log("No encontrado!");
+            console.log(aux);
+            console.log(texto[i]);
+        }
+    }
+}
+
+findFilabyNid(nid, valor);
 
 
 server.listen(process.env.PORT, function() {
