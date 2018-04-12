6479893627f879cc9e74babb2ab9184dfb9b6b8f
Darwin Gonzalez Suarez <alu0100812794@ull.edu.es>
2018-04-12 12:02:26 +0000

    Método addFila añadido

--- methods.js
+++ methods.js
@@ -5,10 +5,18 @@ var fs = require('fs');
 var server = restify.createServer();
 
 var texto ="";
+var nid=100, valor= 'informatica';
 
 var text = fs.readFileSync('data.txt','utf8')
 texto = text.split('\n');
 console.log(texto);
+
+fs.appendFile('data.txt', addFila(nid, valor), 'utf8');
+
+function addFila(nid, valor) {
+    var aux = "{ \"nid\":\"" + nid + "\"," + "\"valor\":\"" + valor + "\" }\n";
+    return aux;
+}
 
 
 server.listen(process.env.PORT, function() {
