1ea4e8a8596d90f5f80a0e4225a5a714caf69f98
Darwin Gonzalez Suarez <alu0100812794@ull.edu.es>
2018-04-12 13:00:16 +0000

    Añadiendo pirncipio eliminarFila :roto2:

--- methods.js
+++ methods.js
@@ -7,6 +7,7 @@ var server = restify.createServer();
 var texto ="";
 var nid = 2, valor = "fisica";
 var nid2 = 7, valor2 = "hola";
+var nid3 = 3, valor3 = "quimica";
 var fila = 0
 
 var text = fs.readFileSync('data.txt','utf8')
@@ -37,7 +38,31 @@ function findFilabyNid(nid, valor) {
     }
 }
 
+function delString(texto) {
+    texto = " ";
+    return texto;
+}
+
+function deleteFila(nid3,valor3) {
+    var text = fs.readFileSync('data.txt','utf8')
+    var texto = text.split('\n');
+    var aux = "{ \"nid\":\"" + nid3 + "\"," + "\"valor\":\"" + valor3 + "\" }";
+    var aux2 = " ";
+    
+    for(var i = 0; i < texto.length ; i++) {
+        if (texto[i].toString() === aux.toString()) {
+            //text[i] = aux;
+            console.log("Eliminado!");
+        }
+        else {
+            console.log("No encontrado!");
+        }
+    }
+}
+
+
 findFilabyNid(nid, valor);
+deleteFila(nid3,valor3);
 
 
 server.listen(process.env.PORT, function() {
